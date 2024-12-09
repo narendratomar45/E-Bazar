3ed7e0de455f4f143ed725461de1a053d93c2fc5
NARENDRA TOMAR <140541020+narendratomar45@users.noreply.github.com>
2024-12-09 15:38:42 +0530

    Deleted frontend files and added backend folder

--- src/store/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "../store/Slices/cartSlice";
-
-export const store = configureStore({
-  reducer: {
-    cartSlice: cartReducer,
-  },
-});
